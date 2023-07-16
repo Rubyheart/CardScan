f62f9ad8fdf3068e09283decf6f370be44d6b534
Stephen <62410139+Bobinstein@users.noreply.github.com>
2023-07-16 11:45:45 -0400

    refactor: modified multifilelook and added .gitignore
    updated multifilelook.py to use relative paths, and remove exact capitalization match needed for search.

--- CardScan/multifilelook.py
+++ CardScan/multifilelook.py
@@ -2,37 +2,30 @@ import os, json
 text = input("Please enter text: ")
 print(f"You have entered \"{text}\" word to search.")
 current_path = os.getcwd()
-current_path
-path = '/home/scott/Documents/CardScan/json/AllSetFiles(1)'
-path
-#global foundeffect, setfile
-#setfile = "set"
-#foundeffect = 'temp'
+path = current_path+'\json\AllSetFiles(1)'
+global foundeffect, setfile
+setfile = "set"
+foundeffect = 'temp'
 effect = "effect"
-os.chdir(path) # Change the current working directory to specified path.
 
-os.listdir(path='.') #
+
 def searchText(path):
-    
-    os.chdir(path)
-    files = os.listdir()
+
+    files = os.listdir(path)
     global foundeffect, setfound
     setfile = 'set'
     effect = 'effect'
-    #foundeffect = ''
-    #setfound = ''
-    #print(files)
+  
     for file_name in files:
-        #print(file_name)
-        abs_path = os.path.abspath(file_name)
-        #print("Absolute path of the file: ", abs_path)
-        with open(file_name, "r") as file:
+        file_path = f'{path}/{file_name}'
+        abs_path = os.path.abspath(file_path)
+        
+        with open(file_path, "r", encoding="utf8") as file:
             data = json.load(file) 
             #print(data)
             for i in data['data']['cards']:
-                #if foundeffect != None:
-                #print(i)
-                    if i['name'] == text:
+                
+                    if i['name'].lower() == text.lower():
                     #break
                         print(i['text'])
                         print("File: ", abs_path)
@@ -61,7 +54,4 @@ def searchText(path):
             searchText(abs_path)
         pass
 searchText(path)
-#effect = effect
-#print(setfile)
-#print(setfound)
 print(effect)
